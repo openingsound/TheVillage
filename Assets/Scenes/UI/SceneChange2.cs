18162f7653c39bd5cfabe905a726f88e3e391e28
rocher71 <qja086351@naver.com>
2020-08-19 03:38:49 +0900

    낚시 게임 제외 모든 씬 연결

---
+++ Assets/Scenes/UI/SceneChange2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange2 : MonoBehaviour
+{
+    public void Option2()
+    {
+        SceneManager.LoadScene("SettingsWindow");
+    }
+
+    public void Forest2()
+    {
+        SceneManager.LoadScene("forestminigame___");
+    }
+    public void toGame2()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+    public void toMiniGame2_1()
+    {
+        SceneManager.LoadScene("MiniGame2");
+    }
+}
