e8843e1c4d129114819c4d4ce1e0a0bd4fa90856
DESKTOP-CBILRSN\ignos <farchicken00@naver.com>
2020-08-25 20:03:10 +0900

    no message

---
+++ Assets/Scenes/02.Scripts/Item/Tree/AppleTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : Item_Tree
+{
+    public override void Planting()
+    {
+        // 오브젝트 이름은 사과나무
+        name = "AppleTree";
+
+        // 사과 나무의 성장 시간은 10초
+        treeGrowTime = 10; 
+
+        // 사과 나무의 열매 성장 시간은 10초
+        fruitGrowTime = 10;
+
+        base.Planting();
+    }
+}
