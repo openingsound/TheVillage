3538bc844d7a185f9465a47bb92729b8cb26cde8
Lee Sun Jae <lsj010320@gmail.com>
2020-09-03 21:56:32 +0900

    밭 작물 성장 애니메이션 추가

---
+++ Assets/MiniGames/FishingGame/Script/Manager/Button1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1 : MonoBehaviour
+{
+    public Button _btn;
+
+    private void Start()
+    {
+        _btn.interactable = false;
+    }
+
+    public void ButtonInteractable(bool i)  //버튼1 비활성화
+    {
+        bool k = i;
+        
+        _btn.interactable = k;
+    }
+}
