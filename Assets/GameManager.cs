4f4df0d3568f05feed8cf347aba2efffc56c0f2a
DESKTOP-GJOVD4L\Openingsound <wlsgus1109@gmail.com>
2020-09-06 17:08:52 +0900

    UI 수정

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -23,6 +23,7 @@ public class Item {
     public int Using;
     public int Cost;
     public int Cycle;
+    public int num;
     public Image image;
     public Item(string type, string name, string tree, string land, string levelst,string isUsing,string cycle)
     {
@@ -38,13 +39,19 @@ public class Item {
 public class GameManager : MonoBehaviour
 {
     public TextAsset ItemDatabase;
-    public List<Item> AllItemList, MyItemList,CurItemList,BuildList;
+    public TextAsset Pre_Cost;
+    public TextAsset Now_Cost;
+    public TextAsset Item_Amount;
+    public List<Item> AllItemList, MyItemList,CurItemList,BuildList,SellList;
+    public List<int> PrePrice,NowPrice,ItemAmount,SellAmount;
     string filePath;
     public GameObject Shop;
     public GameObject[] ShopSlot;
     public GameObject[] BuildSlot;
+    public GameObject[] SellSlot;
     public GameObject ShopPop;
     public GameObject BuildPop;
+    public GameObject SellPop;
     public Text ExplainBox;
     void Start()
     {
@@ -56,6 +63,7 @@ public class GameManager : MonoBehaviour
             //tab으로 나눔
             AllItemList.Add(new Item(row[0], row[1], row[2], row[3], row[4],row[5],row[6]));
         }
+        int n = 0;
         foreach (Item x in AllItemList)
         {
             //print(x.Name+"  : "+ x.Levelst);
@@ -63,17 +71,51 @@ public class GameManager : MonoBehaviour
             x.Using = System.Convert.ToInt32(x.IsUisng);
             x.Cycle = System.Convert.ToInt32(x.SCycle);
             x.Cost = x.level * 10 + x.Cycle/10;
+            x.num = n++;
         }
         filePath = Application.persistentDataPath + "/MyItemText.txt";
         ShopUpdate();
 
+        line = Pre_Cost.text.Substring(0, Pre_Cost.text.Length - 1).Split('\n');
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+            //tab으로 나눔
+            PrePrice.Add(System.Convert.ToInt32(row[0]));
+        }
+        line = Now_Cost.text.Substring(0, Now_Cost.text.Length - 1).Split('\n');
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+            //tab으로 나눔
+            NowPrice.Add(System.Convert.ToInt32(row[0]));
+        }
         //Load();
+        line = Item_Amount.text.Substring(0, Now_Cost.text.Length - 1).Split('\n');
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+            //tab으로 나눔
+            ItemAmount.Add(System.Convert.ToInt32(row[0]));
+            SellAmount.Add(0);
+        }
+    }
+
+
+    public void ChangePrice()
+    {
+        for(int i = 0; i < PrePrice.Count; i++)
+        {
+            PrePrice[i] = NowPrice[i];
+            NowPrice[i] = AllItemList[i].Cost + UnityEngine.Random.Range(-(AllItemList[i].Cost/3), (AllItemList[i].Cost / 3));
+        }
     }
     public void ShopUpdate()
     {
         CurItemList = AllItemList.FindAll(x => x.level <= 3);
         BuildList = AllItemList.FindAll(x => x.Using == 1);
         CurItemList = CurItemList.FindAll(x => x.Using == 0);
+        
         for (int i = 0; i < ShopSlot.Length; i++)
         {
             ShopSlot[i].SetActive(i < CurItemList.Count);
@@ -91,6 +133,77 @@ public class GameManager : MonoBehaviour
             BuildSlot[i].transform.GetChild(2).GetComponentInChildren<Text>().text = System.Convert.ToString((BuildList[i].Cycle * 3) / 60) + "시간 " + ((BuildList[i].Cycle *3 )% 60 == 0 ? "" : System.Convert.ToString(BuildList[i].Cycle *3% 60) + "분");
         }
     }
+
+    public void SellUpdate()
+    {
+        BuildList = AllItemList.FindAll(x => x.Using == 1);
+        CurItemList = CurItemList.FindAll(x => x.Using == 0);
+        for (int i = 0; i < BuildList.Count; i++)
+        {
+            SellSlot[i].SetActive(true);
+            int PPrice = PrePrice[BuildList[i].num];
+            int NPrice = NowPrice[BuildList[i].num];
+            SellSlot[i].transform.GetChild(0).GetComponentInChildren<Text>().text = BuildList[i].Name;//이름
+            SellSlot[i].transform.GetChild(1).GetComponentInChildren<Text>().text = System.Convert.ToString(PPrice);
+            SellSlot[i].transform.GetChild(2).GetComponentInChildren<Text>().text = System.Convert.ToString(NPrice);
+            if (PPrice == NPrice)
+                SellSlot[i].transform.GetChild(3).gameObject.SetActive(false);
+            else {
+                SellSlot[i].transform.GetChild(3).gameObject.SetActive(true);
+                if (PPrice > NPrice)
+                {
+                    SellSlot[i].transform.GetChild(3).GetChild(0).gameObject.SetActive(false);
+                    SellSlot[i].transform.GetChild(3).GetChild(1).gameObject.SetActive(true);
+                    SellSlot[i].transform.GetChild(3).GetComponent<Text>().text = "(         " + System.Convert.ToString(PPrice - NPrice) + " )";
+                }
+                else {
+                    SellSlot[i].transform.GetChild(3).GetChild(0).gameObject.SetActive(true);
+                    SellSlot[i].transform.GetChild(3).GetChild(1).gameObject.SetActive(false);
+                    SellSlot[i].transform.GetChild(3).GetComponent<Text>().text = "(         " + System.Convert.ToString(NPrice - PPrice) + " )";
+                }
+            }
+            SellSlot[i].transform.GetChild(4).GetComponentInChildren<Text>().text = System.Convert.ToString(SellAmount[BuildList[i].num]) +" / " +System.Convert.ToString(ItemAmount[BuildList[i].num]) + " 개";
+        }
+    }
+
+
+
+    public void fun0(int num)
+    {
+        num = BuildList[num].num;
+        SellAmount[num] -= 10;
+        if (SellAmount[num] < 0) SellAmount[num] = 0;
+
+        SellUpdate();
+    }
+    public void fun1(int num)
+    {
+        num = BuildList[num].num;
+        SellAmount[num] -= 1;
+        if (SellAmount[num] < 0) SellAmount[num] = 0;
+
+        SellUpdate();
+    }
+    public void fun2(int num)
+    {
+        num = BuildList[num].num;
+        SellAmount[num] += 1;
+        if (SellAmount[num] > ItemAmount[num]) SellAmount[num] = ItemAmount[num];
+
+        SellUpdate();
+    }
+
+    public void fun3(int num)
+    {
+        
+        num = BuildList[num].num;
+        SellAmount[num] += 10;
+        if (SellAmount[num] > ItemAmount[num]) SellAmount[num] = ItemAmount[num];
+        //print(num +" "+ SellAmount[num]);
+
+        SellUpdate();
+    }
+
     public string curType2;
     public void TabClick(string tabName)
     {
@@ -128,6 +241,20 @@ public class GameManager : MonoBehaviour
     public void CloseBuildPop()
     {
         BuildPop.SetActive(false);
+    }
+    public void OpenSelldPop()
+    {
+        for (int i = 0; i < NowPrice.Count; i++)
+        {
+            SellAmount[i] = 0;
+        }
+        SellUpdate();
+        SellPop.SetActive(true);
+    }
+    public void CloseSellPop()
+    {
+        SellUpdate();
+        SellPop.SetActive(false);
     }
     void Load()
     {
