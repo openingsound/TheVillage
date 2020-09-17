c4cb4750a4e8034628043df4bba2a0b52a671e58
DESKTOP-CBILRSN\ignos <farchicken00@naver.com>
2020-09-17 20:37:07 +0900

    게임오버시 버튼 비활성화와 시간 35초 변경했습니다.

--- Assets/MiniGames/FishingGame/Script/Manager/TimeManager.cs
+++ Assets/MiniGames/FishingGame/Script/Manager/TimeManager.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class TimeManager : MonoBehaviour
 {
 
+    public GameObject _Btn;
 
-    private float done = 80.0f; // 시간 제한 30초
+    private float done = 35.0f; // 시간 제한 35초
 
     public Text _time; //시간 텍스트
 
@@ -15,6 +16,7 @@ public class TimeManager : MonoBehaviour
 
     private void Start()
     {
+        _Btn = GameObject.FindWithTag("Button1");
         _gameOver.SetActive(false);
     }
     void Update()
@@ -31,7 +33,7 @@ public class TimeManager : MonoBehaviour
 
         else
         {
-
+            _Btn.SetActive(false);
             _gameOver.SetActive(true);
 
         }
